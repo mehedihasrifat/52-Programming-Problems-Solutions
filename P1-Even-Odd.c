d6978f96873712a8fcfdd30a36191def29315e6a
mehedihasrifat <ebd.rifat101@gmail.com>
2022-11-29 12:26:28 +0600

    first commit

---
+++ P1-Even-Odd.c
@@ -0,0 +1,36 @@
+
+/**
+ * @file P1-Even-Odd.c
+ * @author Mehedi Hasan Rifat (mehedihasrifat@gmail.com)
+ * @brief This code is actually the solution of the first problem from the book called "52 Programming Problems by Tamim Shahriar Subeen"
+ * @version 0.1
+ * @date 2022-11-29
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int t, n;
+
+    scanf("%d", &t);
+
+    for (int i = 1; i <= t; i++)
+    {
+        scanf("%d", &n);
+
+        if (n % 2 == 0)
+        {
+            printf("even\n");
+        }
+        else
+        {
+            printf("odd\n");
+        }
+    }
+
+    return 0;
+}
